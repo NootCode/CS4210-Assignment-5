3b3b6f2a8edc85faa6084acf23a2eb3944454212
Andre Nalbandian <andrenalbandian@gmail.com>
2021-11-28 21:19:22 -0800

    ARM done

--- ARM/association_rule_mining.py
+++ ARM/association_rule_mining.py
@@ -38,31 +38,43 @@ itemset.remove(np.nan)
 #and when is completed, append the dictionary to the list encoded_vals below (this is done for each transaction)
 #-->add your python code below
 encoded_vals = []
+valCount = [0,0,0,0,0,0,0,0,0]
 for index, row in df.iterrows():
     #key vals[0] = bread; 1 = wine, 2 = eggs, 3 = meat, 4 = cheese
-    # 5 = pencil, 6 = diaper, 7 = bagel
-    vals = [0,0,0,0,0,0,0,0]
+    # 5 = pencil, 6 = diaper, 7 = bagel 8 = milk
+    vals = [0,0,0,0,0,0,0,0,0]
     for i in range(0,7):
         if(row[i] == 'Bread'):
             vals[0] = 1
+            valCount[0]+=1
         if(row[i] == 'Wine'):
             vals[1] = 1
+            valCount[1]+=1
         if(row[i] == 'Eggs'):
             vals[2] = 1
+            valCount[2]+=1
         if(row[i] == 'Meat'):
             vals[3] = 1
+            valCount[3]+=1
         if(row[i] == 'Cheese'):
             vals[4] = 1
+            valCount[4]+=1
         if(row[i] == 'Pencil'):
             vals[5] = 1
+            valCount[5]+=1
         if(row[i] == 'Diaper'):
             vals[6] = 1
+            valCount[6]+=1
         if(row[i] == 'Bagel'):
             vals[7] = 1
+            valCount[7]+=1
+        if(row[i] == 'Milk'):
+            vals[8] = 1
+            valCount[8]+=1
 
     labels = {'Bread' : vals[0], 'Wine' : vals[1], 'Eggs': vals[2], 
     'Meat' : vals[3], 'Cheese': vals[4],'Pencil': vals[5], 
-    'Diaper': vals[6], 'Bagel': vals[7]}
+    'Diaper': vals[6], 'Bagel': vals[7], 'Milk' : vals[8]}
 
     encoded_vals.append(labels)
 
@@ -81,22 +93,43 @@ rules = association_rules(freq_items, metric="confidence", min_threshold=0.6)
 #Prior: 0.4380952380952381
 #Gain in Confidence: 52.17391304347825
 #-->add your python code below
-print(rules)
+print(valCount)
 for index,row in rules.iterrows():
     #print(str(row['antecedents']) + " -> " + str(row['consequents']))
+    suportCount = 0
     for x in row['antecedents']:
         print(str(x) + " ", end="")
-    print(" -> ", end= "")
+    print("-> ", end= "")
     for x in row['consequents']:
         print(str(x) + " ", end = "")
+        if (x == 'Bread'):
+            suportCount += valCount[0]
+        if (x == 'Wine'):
+            suportCount += valCount[1]
+        if (x == 'Eggs'):
+            suportCount += valCount[2]
+        if (x == 'Meat'):
+            suportCount += valCount[3]
+        if (x == 'Cheese'):
+            suportCount += valCount[4]
+        if (x == 'Pencil'):
+            suportCount += valCount[5]
+        if (x == 'Diaper'):
+            suportCount += valCount[6]
+        if (x == 'Bagel'):
+            suportCount += valCount[7]
+        if (x == 'Milk'):
+            suportCount += valCount[8]
     print()
     print("Support: " + str(row['support'])) # support
     print("Confidence: " + str(row['confidence'])) #confidence
 #To calculate the prior and gain in confidence, find in how many transactions the consequent of the rule appears (the supporCount below). Then,
 #use the gain formula provided right after.
-#prior = suportCount/len(encoded_vals) -> encoded_vals is the number of transactions
-#print("Gain in Confidence: " + str(100*(rule_confidence-prior)/prior))
+    prior = suportCount/len(encoded_vals) #-> encoded_vals is the number of transactions
+    print("Prior: " + str(prior))
+    print("Gain in Confidence: " + str(100*(row['confidence']-prior)/prior))
 #-->add your python code below
+    print()
 
 #Finally, plot support x confidence
 
